db3d531f7da0d20fd7dbfa893c4a8679e7417d23
abi franklin <abifranklin@gmail.com>
2020-07-19 08:23:39 -0500

    General file structure complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import QuestionCard from './Components/QuestionCard';
  
-function App() {
+const App = () => {
+  const startTrivia = async() => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  
+  const nextQuestion = () => {};
+
   return (
-    <div className="App">
-      Quiz
-    </div>
+  <div className="App">
+    <h1>General Knowledge Quiz</h1>
+    <h3>Build with React & TS by Abi Franklin</h3>
+    <button className="start" onClick={startTrivia}>
+      Start Quiz
+    </button>
+    <br/>
+    <QuestionCard />
+    <br />
+    <span>Photo by <a href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Paweł Czerwiński</a> on <a href="https://unsplash.com/t/textures-patterns?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+  </div>
   );
 }
 
