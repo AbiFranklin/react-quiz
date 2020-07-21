8f76f15fdb28b38708530bc489fa1fc4d2a552c2
abi franklin <abifranklin@gmail.com>
2020-07-20 20:26:12 -0500

    Final tutorial version complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { fetchQuizQuestions, Difficulty, QuestionState } from './API'
 import QuestionCard from './Components/QuestionCard'
+import { GlobalStyle, Wrapper } from './App.styles'
 
-type AnswerObject = {
+export type AnswerObject = {
     question: string
     answer: string
     correct: boolean
@@ -63,50 +64,58 @@ const App = () => {
     }
 
     return (
-        <div className="App">
-            <h1>General Knowledge Quiz</h1>
-            <h3>Build with React & TS by Abi Franklin</h3>
-            {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-                <button className="start" onClick={startTrivia}>
-                    Start Quiz
-                </button>
-            ) : null}
-            {!gameOver ? (
-                <p className="score">
-                    Score: {score}/{TOTAL_QUESTIONS}{' '}
-                </p>
-            ) : null}
-            {loading && <p>Loading Questions ...</p>}
-            {!loading && !gameOver && (
-                <QuestionCard
-                    questionNum={number + 1}
-                    totalQuestions={TOTAL_QUESTIONS}
-                    question={questions[number].question}
-                    answers={questions[number].answers}
-                    userAnswer={userAnswers ? userAnswers[number] : undefined}
-                    callback={checkAnswer}
-                />
-            )}
-            {!gameOver &&
-            !loading &&
-            userAnswers.length === number + 1 &&
-            number !== TOTAL_QUESTIONS - 1 ? (
-                <button className="next" onClick={nextQuestion}>
-                    Next Question
-                </button>
-            ) : null}
-            <br />
-            <span>
-                Background by{' '}
-                <a href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-                    Paweł Czerwiński
-                </a>{' '}
-                on{' '}
-                <a href="https://unsplash.com/t/textures-patterns?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-                    Unsplash
-                </a>
-            </span>
-        </div>
+        <>
+            <GlobalStyle />
+            <Wrapper>
+                <h1>General Knowledge Quiz</h1>
+                <h3>
+                    Build with React & TS by{' '}
+                    <a href="mailto:abifranklin@gmail.com"> Abi Franklin </a>
+                </h3>
+                {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+                    <button className="start" onClick={startTrivia}>
+                        Start Quiz
+                    </button>
+                ) : null}
+                {!gameOver ? (
+                    <p className="score">
+                        Score: {score}/{TOTAL_QUESTIONS}{' '}
+                    </p>
+                ) : null}
+                {loading && <p>Loading Questions ...</p>}
+                {!loading && !gameOver && (
+                    <QuestionCard
+                        questionNum={number + 1}
+                        totalQuestions={TOTAL_QUESTIONS}
+                        question={questions[number].question}
+                        answers={questions[number].answers}
+                        userAnswer={
+                            userAnswers ? userAnswers[number] : undefined
+                        }
+                        callback={checkAnswer}
+                    />
+                )}
+                {!gameOver &&
+                !loading &&
+                userAnswers.length === number + 1 &&
+                number !== TOTAL_QUESTIONS - 1 ? (
+                    <button className="next" onClick={nextQuestion}>
+                        Next Question
+                    </button>
+                ) : null}
+                <br />
+                <span className="footer">
+                    Background by{' '}
+                    <a href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                        Paweł Czerwiński
+                    </a>{' '}
+                    on{' '}
+                    <a href="https://unsplash.com/t/textures-patterns?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                        Unsplash
+                    </a>
+                </span>
+            </Wrapper>
+        </>
     )
 }
 
--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { type } from 'os'
+import { AnswerObject } from '../App'
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
 
 type Props = {
     question: string
     answers: string[]
-    callback: any
-    userAnswer: any
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void
+    userAnswer: AnswerObject | undefined
     questionNum: number
     totalQuestions: number
 }
@@ -18,25 +19,29 @@ const QuestionCard: React.FC<Props> = ({
     questionNum,
     totalQuestions,
 }) => (
-    <div>
+    <Wrapper>
         <p className="number">
             Question: {questionNum} / {totalQuestions}
         </p>
         <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
             {answers.map((answer) => (
-                <div key={answer}>
+                <ButtonWrapper
+                    key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}
+                >
                     <button
-                        disabled={userAnswer}
+                        disabled={!!userAnswer}
                         value={answer}
                         onClick={callback}
                     >
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div>
+    </Wrapper>
 )
 
 export default QuestionCard
